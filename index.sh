b0e444dd8d8941063dc1621d196c496ff34ac887
NGROKVPS123 <149353524+NGROKVPS123@users.noreply.github.com>
2024-05-18 19:45:47 +0700

    Update index.sh

--- index.sh
+++ index.sh
@@ -9,23 +9,26 @@ wget -O bios64.bin "https://github.com/BlankOn/ovmf-blobs/raw/master/bios64.bin"
 # Download ngrok
 wget -O ngrok.tgz "https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz"
 
-# Download Windows ISO
-wget -O win.iso "https://drive.massgrave.dev/en-us_windows_server_2022_updated_dec_2023_x64_dvd_f101ef8f.iso"
+# Download Windows 10 LTSB ISO
+wget -O Windows10LTSB.iso "https://computernewb.com/isos/windows/Windows%2010%20LTSB%20x64.iso"
 
 # Extract ngrok
 tar -xf ngrok.tgz
 
 # Set ngrok authtoken
-./ngrok authtoken 2dkWMMUBRS6BZ389V7KvCJAlXR1_6aSGzkugyG2aQDizGkFPb
+./ngrok authtoken 2e2RaW4cZ5FSf1NPEee1qSBrqr7_6fyLW3fisrJ3HztrrptTN
 
 # Start ngrok tunnel
 ./ngrok tcp 5900
+
+#Update
+sudo apt update -y
 
 # Install qemu-kvm
 sudo apt install qemu-kvm -y
 
 # Create a raw disk image for Windows
-qemu-img create -f raw win.img 25G
+qemu-img create -f raw win.img 64G
 
 # Run QEMU with specified parameters
-sudo qemu-system-x86_64 -m 8G -smp 2 -cpu host -boot order=c -drive file=win.iso,media=cdrom -drive file=win.img,format=raw -device usb-ehci,id=usb,bus=pci.0,addr=0x4 -device usb-tablet -vnc :0 -smp cores=2 -device e1000,netdev=n0 -netdev user,id=n0 -vga qxl -accel kvm -bios bios64.bin
+sudo qemu-system-x86_64 -m 8G -smp 2 -cpu host -boot order=c -drive file=Windows10LTSB.iso,media=cdrom -drive file=win.img,format=raw -device usb-ehci,id=usb,bus=pci.0,addr=0x4 -device usb-tablet -vnc :0 -smp cores=2 -device e1000,netdev=n0 -netdev user,id=n0 -vga qxl -accel kvm -bios bios64.bin
